1de6d762f51a5bec974df7b760a6d133208ce911
Felippe Nobre <113641116+Felippe-nobre@users.noreply.github.com>
2024-10-08 11:49:48 -0300

    .

--- aulas/aula07/ambiente.js
+++ aulas/aula07/ambiente.js
@@ -1,19 +1,6 @@
 var c = 1
-do{
-    console.log(`Passo ${c}`)
-    c++
-} while (c <= 6)
 
-/*while (c <= 4) {
+while (c <= 4) {
     console.log(`passo ${c}`)
     c++
-} */
-/*
-pior forma
- console.log('tudo bem?')
- console.log('tudo bem?')
- console.log('tudo bem?')
- console.log('tudo bem?')
- console.log('tudo bem?')
- console.log('tudo bem?')
- */
+} 
